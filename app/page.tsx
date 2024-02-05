2d3f2f467446731f94e903f2d6231d80b8da664b
Aditya Donkada <adityadonkada@hotmail.com>
2024-02-05 13:22:02 -0800

    fix: unlike-any-tool section gone

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { Clients } from "../components/sections/clients";
 import { EnjoyIssueTracking } from "../components/sections/enjoy-issue-tracking";
 import { HomepageHero } from "../components/sections/homepage-hero";
 import { SetDirection } from "../components/sections/set-direction";
-import { UnlikeAnyTool } from "../components/sections/unlike-any-tool";
 
 export default function Homepage() {
   return (
